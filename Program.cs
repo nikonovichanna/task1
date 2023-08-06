82b5c031ada3331ad03a5f9e9ef91beee2db8800
nikonovichanna <nikonovicvhhanna@gmail.com>
2023-08-06 23:37:46 +0300

    add task1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+if (numberA > numberB)
+{
+    Console.WriteLine(numberA);
+}
+else
+{
+    Console.WriteLine(numberB);
+}
