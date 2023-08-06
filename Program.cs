4db022427f02a3678ed3886ea57d99e39d8d5dec
nikonovichanna <nikonovicvhhanna@gmail.com>
2023-08-07 00:09:04 +0300

    add task1

--- Program.cs
+++ Program.cs
@@ -2,16 +2,11 @@
 Console.WriteLine(numberA);
 int numberB = new Random().Next(1,10);
 Console.WriteLine(numberB);
-int numberC = new Random().Next(1,10);
-Console.WriteLine(numberC);
-int max = numberA;
-if (numberB > max)
+if (numberA > numberB)
 {
-    max = numberB;
+    Console.WriteLine(numberA);
 }
 else
-    if (numberC > max)
-    {
-    max = numberC;
-    }
-System.Console.WriteLine(max);
+{
+    Console.WriteLine(numberB);
+}
